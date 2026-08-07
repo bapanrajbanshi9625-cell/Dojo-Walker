e544af2aa92c7ebbe179e17ae982298f1efbb961
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-08 01:51:32 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'features/auth/presentation/mobile_login_screen.dart';
-import 'features/walk_tracking/presentation/main_navigation_screen.dart';
+import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const DojoWalkerApp());
-}
-
-class DojoWalkerApp extends StatelessWidget {
-  const DojoWalkerApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dojo Walker - Buddy',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              backgroundColor: Colors.white,
-              body: Center(
-                child: CircularProgressIndicator(
-                  color: Colors.orange,
-                ),
-              ),
-            );
-          }
-
-          if (snapshot.hasData && snapshot.data != null) {
-            return const MainNavigationScreen();
-          } 
-          
-          return const MobileLoginScreen();
-        },
-      ),
-    );
-  }
 }
