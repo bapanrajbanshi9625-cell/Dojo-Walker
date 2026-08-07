13ccb1f7cc00999c36894e3b68beede8b945ff65
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-07 19:01:44 +0530

    Translate color comments to English
    Updated comments for primary and secondary colors to English.

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFFFF6600); // ब्राइट प्राइमरी ऑरेंज
-  static const Color secondary = Color(0xFF1E3A8A); // सेकेंडरी रॉयल ब्लू
+  static const Color primary = Color(0xFFFF6600); // Bright Primary Orange
+  static const Color secondary = Color(0xFF1E3A8A); // Royal Blue
   static const Color scaffoldBackground = Color(0xFFF9FAFB);
   static const Color textDark = Color(0xFF1F2937);
   static const Color textGrey = Color(0xFF6B7280);
