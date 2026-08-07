223ab1c8c9c62b97391cbca1bc873e1e84ad07e7
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-07 18:58:07 +0530

    Add AppColors class with color constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFFF6600); // ब्राइट प्राइमरी ऑरेंज
+  static const Color secondary = Color(0xFF1E3A8A); // सेकेंडरी रॉयल ब्लू
+  static const Color scaffoldBackground = Color(0xFFF9FAFB);
+  static const Color textDark = Color(0xFF1F2937);
+  static const Color textGrey = Color(0xFF6B7280);
+}
