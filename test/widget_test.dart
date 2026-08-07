8266efdb96f2e17d8dfc0af1e4165d441d311c46
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-08 02:04:58 +0530

    Remove unused imports in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:dojo_walker/app.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
