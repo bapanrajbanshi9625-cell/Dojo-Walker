33684f852f3266abab8223ed89afbb4a44719484
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-13 14:53:10 +0530

    Create past_walks_container.dart

---
+++ lib/features/walker_home/containers/past_walks_container.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../walker_home_features.dart';
+import '../widgets/section_title.dart';
+import '../widgets/past_walk_card.dart';
+
+class PastWalksContainer extends StatelessWidget {
+  final void Function({
+    required String title,
+    required String description,
+    required IconData icon,
+  }) onDetails;
+
+  const PastWalksContainer({
+    super.key,
+    required this.onDetails,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment:
+          CrossAxisAlignment.start,
+      children: [
+        const WalkerSectionTitle(
+          title: 'Past Walks',
+        ),
+
+        const SizedBox(height: 12),
+
+        ...WalkerHomeFeatures.pastWalks.map(
+          (walk) {
+            return Padding(
+              padding:
+                  const EdgeInsets.only(bottom: 10),
+              child: PastWalkCard(
+                id: walk['id']!,
+                time: walk['time']!,
+                details: walk['details']!,
+                onTap: () {
+                  onDetails(
+                    title: 'Walk ${walk['id']}',
+                    icon: Icons.pets_rounded,
+                    description:
+                        WalkerHomeFeatures
+                            .pastWalkDetails(walk),
+                  );
+                },
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
