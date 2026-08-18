a186f7b5c5b3e391e14013406f98b12744e62d5d
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-18 12:20:24 +0530

    Remove unused import for app_colors.dart

--- lib/screens/otp_verification_screen.dart
+++ lib/screens/otp_verification_screen.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import '../core/constants/app_colors.dart';
 import '../features/profile_setup/services/profile_setup_service.dart';
 import '../services/auth_service.dart';
 import '../services/walker_id_service.dart';
