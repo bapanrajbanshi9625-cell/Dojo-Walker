5cd10973fb3423da7e55f5f5ec44be28aaf273af
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-19 15:20:59 +0530

    Remove unused import for profile_setup_service

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../features/profile_setup/services/profile_setup_service.dart';
 import '../features/profile_setup/screens/pending_verification_screen.dart';
 
 import 'main_navigation_screen.dart';
