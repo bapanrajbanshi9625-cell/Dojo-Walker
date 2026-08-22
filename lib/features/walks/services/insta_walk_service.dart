fd5673103927a18129e2f71b65e63b8207c21302
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-23 03:37:54 +0530

    Add geolocator import to insta_walk_service.dart

--- lib/features/walks/services/insta_walk_service.dart
+++ lib/features/walks/services/insta_walk_service.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
 
 import '../constants/walks_constants.dart';
 import '../models/walk_request.dart';
