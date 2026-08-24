b415ee3bee6c8da63fae37d6b36fae0c14d5f64c
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-24 10:48:09 +0530

    Update import path for walks_constants.dart

--- lib/features/insta_walk/widgets/insta_walk_search_button.dart
+++ lib/features/insta_walk/widgets/insta_walk_search_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../constants/walks_constants.dart';
+import '../../walks/constants/walks_constants.dart';
 
 class InstaWalkSearchButton extends StatelessWidget {
   final bool loading;
