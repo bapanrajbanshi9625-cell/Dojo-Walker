f8155dc8012d0e49449257160677c2b9a8f17f55
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-24 18:38:09 +0530

    Update active_walk_details_screen.dart

--- lib/features/insta_walk/screens/active_walk_details_screen.dart
+++ lib/features/insta_walk/screens/active_walk_details_screen.dart
@@ -3,6 +3,7 @@
 
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
