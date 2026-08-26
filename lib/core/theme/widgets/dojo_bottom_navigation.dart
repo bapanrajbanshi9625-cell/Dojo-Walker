6ed8b93262312698c2efe8b001ae15a553922334
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-27 00:37:11 +0530

    Update dojo_bottom_navigation.dart

--- lib/core/theme/widgets/dojo_bottom_navigation.dart
+++ lib/core/theme/widgets/dojo_bottom_navigation.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 import '../dojo_walker_colors.dart';
 
-class DojoBottomNavigation
-    extends StatelessWidget {
+class DojoBottomNavigation extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+  final List<BottomNavigationBarItem> items;
+
   const DojoBottomNavigation({
     super.key,
     required this.currentIndex,
@@ -11,39 +14,39 @@ class DojoBottomNavigation
     required this.items,
   });
 
-  final int currentIndex;
-  final ValueChanged<int> onTap;
-  final List<BottomNavigationBarItem> items;
-
   @override
-Widget build(BuildContext context) {
-  return Container(
-    decoration: const BoxDecoration(
-      color: DojoWalkerColors.white,
-      border: Border(
-        top: BorderSide(
-          color: DojoWalkerColors.border,
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: DojoWalkerColors.white,
+        border: Border(
+          top: BorderSide(
+            color: DojoWalkerColors.border,
+          ),
         ),
       ),
-    ),
-    child: NavigationBar(
-      selectedIndex: currentIndex,
-      onDestinationSelected: onTap,
-      backgroundColor: DojoWalkerColors.white,
-      indicatorColor: DojoWalkerColors.primarySoft,
-      elevation: 0,
-      height: 70,
-      destinations: items
-          .map(
-            (BottomNavigationBarItem item) {
-              return NavigationDestination(
-                icon: item.icon,
-                selectedIcon: item.activeIcon,
-                label: item.label ?? '',
-              );
-            },
-          )
-          .toList(),
-    ),
-  );
+      child: NavigationBar(
+        selectedIndex: currentIndex,
+        onDestinationSelected: onTap,
+        backgroundColor:
+            DojoWalkerColors.white,
+        indicatorColor:
+            DojoWalkerColors.primarySoft,
+        elevation: 0,
+        height: 70,
+        destinations: items
+            .map(
+              (BottomNavigationBarItem item) {
+                return NavigationDestination(
+                  icon: item.icon,
+                  selectedIcon:
+                      item.activeIcon ?? item.icon,
+                  label: item.label ?? '',
+                );
+              },
+            )
+            .toList(),
+      ),
+    );
+  }
 }
