4bf9416ac9982ed6c84862c64cce3a634b2ffb05
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-26 14:06:28 +0530

    Create ActiveWalkAddressCard widget

---
+++ lib/features/insta_walk/widgets/active_walk_address_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/app_colors.dart';
+
+class ActiveWalkAddressCard extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String value;
+
+  const ActiveWalkAddressCard({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.value,
+  });
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return Container(
+      padding:
+          const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: AppColors.surfaceSoft,
+        borderRadius:
+            BorderRadius.circular(15),
+      ),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            color: AppColors.primary,
+            size: 17,
+          ),
+          const SizedBox(width: 7),
+          Expanded(
+            child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: AppColors.textMuted,
+                    fontSize: 7,
+                    fontWeight:
+                        FontWeight.w900,
+                  ),
+                ),
+                const SizedBox(height: 3),
+                Text(
+                  value,
+                  maxLines: 2,
+                  overflow:
+                      TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: AppColors.secondary,
+                    fontSize: 9,
+                    fontWeight:
+                        FontWeight.w800,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
