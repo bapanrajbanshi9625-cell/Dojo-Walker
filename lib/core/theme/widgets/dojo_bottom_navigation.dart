b651528efb85d71b781718fb1ee2f73d5f2e298e
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-27 00:42:26 +0530

    Update dojo_bottom_navigation.dart

--- lib/core/theme/widgets/dojo_bottom_navigation.dart
+++ lib/core/theme/widgets/dojo_bottom_navigation.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 import '../dojo_walker_colors.dart';
 
 class DojoBottomNavigation extends StatelessWidget {
-  final int currentIndex;
-  final ValueChanged<int> onTap;
-  final List<BottomNavigationBarItem> items;
-
   const DojoBottomNavigation({
     super.key,
     required this.currentIndex,
     required this.onTap,
     required this.items,
   });
+
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+  final List<BottomNavigationBarItem> items;
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +28,8 @@ class DojoBottomNavigation extends StatelessWidget {
       child: NavigationBar(
         selectedIndex: currentIndex,
         onDestinationSelected: onTap,
-        backgroundColor:
-            DojoWalkerColors.white,
-        indicatorColor:
-            DojoWalkerColors.primarySoft,
+        backgroundColor: DojoWalkerColors.white,
+        indicatorColor: DojoWalkerColors.primarySoft,
         elevation: 0,
         height: 70,
         destinations: items
@@ -39,8 +37,7 @@ class DojoBottomNavigation extends StatelessWidget {
               (BottomNavigationBarItem item) {
                 return NavigationDestination(
                   icon: item.icon,
-                  selectedIcon:
-                      item.activeIcon ?? item.icon,
+                  selectedIcon: item.activeIcon,
                   label: item.label ?? '',
                 );
               },
