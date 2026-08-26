3364976351a9f2bf862da49d602b6ed941daf231
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-27 01:51:52 +0530

    Add PanCard2 widget for displaying PAN card details

--- lib/features/profile_setup/widgets/pan_card2.dart
+++ lib/features/profile_setup/widgets/pan_card2.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+import '../../../core/constants/app_colors.dart';
+import 'document_card2.dart';
+
+class PanCard2 extends StatelessWidget {
+  const PanCard2({
+    super.key,
+    required this.url,
+    required this.onTap,
+    required this.enabled,
+  });
+
+  final String? url;
+  final VoidCallback onTap;
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return DocumentCard2(
+      title: 'PAN Card',
+      subtitle: 'Testing के लिए Image URL',
+      url: url,
+      onTap: enabled ? onTap : null,
+      accentColor: AppColors.orange,
+      icon: Icons.credit_card_rounded,
+    );
+  }
+}
