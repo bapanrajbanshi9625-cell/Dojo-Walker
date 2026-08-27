9a4b6b0106b8856b63f3c70b3d310af04e311e63
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-28 02:09:38 +0530

    Update color opacity method in splash_screen.dart
    Refactor splash screen to use withValues for color opacity.

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -5,7 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../core/constants/app_colors.dart';
 import '../features/walker_verification/screens/pending_verification_screen.dart';
 import 'main_navigation_screen.dart';
 import 'mobile_login_screen.dart';
@@ -625,11 +624,11 @@ class _SplashScreenState extends State<SplashScreen> {
           // ======================================================
 
           Container(
-            color: backgroundColor.withOpacity(
-              0.08,
-            ),
+           color: backgroundColor.withValues(
+            alpha: 0.08,
+           ),
           ),
-
+          
           // ======================================================
           // STARTUP STATUS
           // ======================================================
