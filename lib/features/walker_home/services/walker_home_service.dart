e79902ecbb7154aa69e3dba911f1be9ff7297eaa
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-09-01 12:20:04 +0530

    Update import path for past_walk_model.dart

--- lib/features/walker_home/services/walker_home_service.dart
+++ lib/features/walker_home/services/walker_home_service.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-import '../models/past_walk_model.dart';
+import '../../my_walks/models/past_walk_model.dart';
 
 class WalkerHomeService {
   WalkerHomeService({
