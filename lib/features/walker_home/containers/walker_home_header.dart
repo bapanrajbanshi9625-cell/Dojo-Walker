3a853e1dd69fa2c0e03e9b932e775523a7b7dc9d
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-09-04 13:50:36 +0530

    Update walker_home_header.dart

--- lib/features/walker_home/containers/walker_home_header.dart
+++ lib/features/walker_home/containers/walker_home_header.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 import '../../../core/theme/dojo_walker_colors.dart';
 import '../../../screens/help_support_screen.dart';
 import '../../../screens/notifications_screen.dart';
-import '../../../screens/profile_screen.dart';
+import '../../profile/screens/profile_screen.dart';
 
 class WalkerHomeHeader extends StatelessWidget {
   const WalkerHomeHeader({super.key});
