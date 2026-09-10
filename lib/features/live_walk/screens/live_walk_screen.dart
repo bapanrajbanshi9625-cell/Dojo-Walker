85f4bc325a8bd202dc0a269138bfb95065d9afa7
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-09-11 00:34:23 +0530

    Refactor LiveWalkScreen to include session controller

--- lib/features/live_walk/screens/live_walk_screen.dart
+++ lib/features/live_walk/screens/live_walk_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../core/constants/app_colors.dart';
-import '../../contacts/screens/chat_sheet.dart';
+import '../controllers/live_walk_session_controller.dart';
 import '../widgets/live_walk_bottom_sheet.dart';
 import '../widgets/live_walk_map_layer.dart';
 
@@ -302,6 +302,7 @@ class _LiveWalkScreenState extends State<LiveWalkScreen> {
     return LiveWalkBottomSheet(
       scrollController: scrollController,
       ending: ending,
+      ownerUid: widget.ownerUid,
       ownerName: widget.ownerName,
       dogName: widget.dogName,
       dogBreed: widget.dogBreed,
